1323b76abdb1c97bac6b2096c48bc305740d340c
cemsoto7 <102704110+cemsoto7@users.noreply.github.com>
2022-03-30 18:07:48 -0600

    Add files via upload

---
+++ js/index-scrollreveal.js
@@ -0,0 +1,47 @@
+ScrollReveal().reveal('.carousel', {
+   duration: 1500,
+   origin: 'bottom',
+   distance: '-100px'
+});
+ScrollReveal().reveal('.revel-servicios', { 
+   duration: 2500,
+   origin: 'left',
+   distance: '-100px'
+});
+ScrollReveal().reveal('.revel-clientes', { 
+   duration: 2500,
+   origin: 'right',
+   distance: '-100px'
+});
+ScrollReveal().reveal('.revel-proyectos', { 
+   duration: 2500,
+   origin: 'bottom',
+   distance: '-100px'
+});
+ScrollReveal().reveal('.revel-precios', { 
+   duration: 2500,
+   origin: 'right',
+   distance: '-100px'
+});
+ScrollReveal().reveal('.revel-contacto-mapa', { 
+   duration: 1000,
+   origin: 'left',
+   distance: '-150px'
+});
+ScrollReveal().reveal('.revel-preguntas', { 
+   duration: 2500,
+   origin: 'right',
+   distance: '-150px'
+});
+ScrollReveal().reveal('.revel-banner', { 
+   duration: 2500,
+   origin: 'bottom',
+   distance: '-150px'
+});
+ScrollReveal().reveal('.texto-quienes', { 
+   duration: 2500,
+   origin: 'left',
+   distance: '-150px'
+});
+
+
